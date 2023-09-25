2f06c16d0015e7517132212f15bc943121e3f410
LilyanGuedes <108057303+LilyanGuedes@users.noreply.github.com>
2023-09-24 23:01:11 -0300

    feat model

---
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,51 @@
+package com.example.springboot.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import  java.util.UUID;
+
+@Entity
+@Table(name = "TB_PRODUCTS")
+public class ProductModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduto;
+    private String name;
+    private BigDecimal value;
+
+
+    public UUID getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(UUID idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}
